d9426c04820070e23586db17e03997e5c32a7485
Renato Chaves <51747413+Renato-Chaves@users.noreply.github.com>
2022-03-08 15:44:40 -0300

    Added a Login page, and a Register page.

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/indexStyle.css">
+  <title>Login</title>
+</head>
+<body>
+  
+  <div class='container'>
+
+    <div id='title'>
+      <h1>Login</h1>
+    </div>
+
+    <hr>
+
+    <input type="text" name="login" class="inputs" id="login" placeholder="Login"><br>
+    <input type="text" name="pass" class="inputs" id="pass" placeholder="Password"><br>
+    <a href="register.php"><input type="submit" class="btn" id="registerBtn" value="Register"></a>
+    <input type="submit" class="btn" id="loginBtn" value="Login">
+
+  </div>
+
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/registerStyle.css">
+  <title>Register</title>
+</head>
+<body>
+  
+  <div class='container'>
+
+    <div id='title'>
+      <h1>Register</h1>
+    </div>
+
+    <hr>
+
+    <input type="text" name="username" class="inputs" id="username" placeholder="Username"><br>
+    <input type="text" name="login" class="inputs" id="login" placeholder="Login"><br>
+    <input type="text" name="pass" class="inputs" id="pass" placeholder="Password"><br>
+    <a href="index.php"><input type="submit" class="btn" id="loginBtn" value="Login"></a>
+    <input type="submit" class="btn" id="registerBtn" value="Register">
+
+  </div>
+
+</body>
+</html>
